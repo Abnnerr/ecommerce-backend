5e4940da9b1900936c2a56f16cb9d72c5d339d21
fbmcarmo <fbrunomcarmo@gmail.com>
2026-03-07 19:01:06 -0300

    feature: validate-types-formats-products

--- src/modules/products/product.routes.js
+++ src/modules/products/product.routes.js
@@ -1,18 +1,39 @@
 const { Router } = require("express");
 const ProductController = require("./product.controller");
 const { rotaProtegida } = require("../../shared/middlewares/token.middleware");
+const { validarProduto } = require("../../shared/middlewares/product.middleware");
 
 const router = Router();
 
 router.get("/", ProductController.getAll);
 router.get("/:id", ProductController.getById);
 
-router.post("/", rotaProtegida, ProductController.create);
-router.put("/:id", rotaProtegida, ProductController.update);
+router.post(
+  "/",
+  rotaProtegida,
+  validarProduto,
+  ProductController.create
+);
+
+router.put(
+  "/:id",
+  rotaProtegida,
+  validarProduto,
+  ProductController.update
+);
+
 router.delete("/:id", rotaProtegida, ProductController.delete);
 
-router.put("/:id/tamanhos", rotaProtegida, ProductController.atualizarTamanhos);
+router.put(
+  "/:id/tamanhos",
+  rotaProtegida,
+  ProductController.atualizarTamanhos
+);
 
-router.put("/:id/cores", rotaProtegida, ProductController.atualizarCores);
+router.put(
+  "/:id/cores",
+  rotaProtegida,
+  ProductController.atualizarCores
+);
 
 module.exports = router;
