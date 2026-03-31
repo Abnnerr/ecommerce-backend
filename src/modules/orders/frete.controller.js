f853cb31806d091d06e16c5da0404e8dceccc85f
Abnnerr <jonnas.ab.00@gmail.com>
2026-03-31 06:29:26 -0300

    updates

--- src/modules/orders/frete.controller.js
+++ src/modules/orders/frete.controller.js
@@ -1,5 +1,6 @@
 class FreteController {
   static async calcular(req, res) {
+    console.log(req.body)
     try {
       const response = await fetch("https://sandbox.melhorenvio.com.br/api/v2/me/shipment/calculate", {
         method: "POST",
@@ -7,11 +8,27 @@ class FreteController {
           "Content-Type": "application/json",
           "Authorization": `Bearer ${process.env.MELHOR_ENVIO_TOKEN}` // ← TOKEN NO .ENV
         },
-        body: JSON.stringify(req.body)
+        body: JSON.stringify({
+          from: {
+            postal_code: "01001000"
+          },
+          to: {
+            postal_code: req.body.cep
+          },
+          products: req.body.products.map(item => ({
+            id: item.id,
+            width: item.width,
+            height: item.height,
+            length: item.length,
+            weight: item.weight,
+            insurance_value: Number(item.insurance_value),
+            quantity: item.quantity
+          }))
+        })
       });
 
       const fretes = await response.json();
-      
+
       if (!response.ok) {
         return res.status(response.status).json(fretes);
       }
@@ -19,9 +36,9 @@ class FreteController {
       res.json(fretes);
     } catch (error) {
       console.error("Erro no cálculo de frete:", error);
-      res.status(500).json({ 
+      res.status(500).json({
         erro: "Erro ao calcular frete",
-        detalhes: error.message 
+        detalhes: error.message
       });
     }
   }
